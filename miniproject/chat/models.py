f2fda4c0292e31871b8b8be6bd273384fc6b1480
Sahil <sahilsingh7977@gmail.com>
2023-02-19 16:19:05 +0530

    added tables

--- miniproject/chat/models.py
+++ miniproject/chat/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class helper (models.Model):
+    group_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=20)
