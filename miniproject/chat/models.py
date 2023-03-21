13d1fe439aba89ddf5079afbbfda339989bfee64
Merwin <merwinrebello02@gmail.com>
2023-03-21 21:25:57 +0530

    username on incoming message and background image issue solved

--- miniproject/chat/models.py
+++ miniproject/chat/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class helper (models.Model):
     group_name = models.CharField(max_length=50)
     user_name = models.CharField(max_length=20)
+    #random_img = models.ImageField(upload_to='C:\Users\Oldin Rebello\Pictures\Screenshots')
+    # max size should be 35kb as random image generated is of around 30kbs
---
+++ temp.py
@@ -0,0 +1,4 @@
+from randimage import get_random_image,show_array
+img_size=(128,128)
+img= get_random_image(img_size)
+show_array(img)
